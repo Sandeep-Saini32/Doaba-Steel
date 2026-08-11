bd9c5d29f9ba3f0e37b5e188b675e1ab7778dfa7
Sandeep-Saini32 <sandeep.saini6712@gmail.com>
2026-08-11 15:32:24 +0530

    update

--- frontend/src/components/userheader.js
+++ frontend/src/components/userheader.js
@@ -1,14 +1,19 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { Usercontext } from "./usercontext"
 
 export const Userheader=()=>{
 
     const navigate=useNavigate()
+    
     const {setrole}=useContext(Usercontext)
+
+    const [sidebaropen,setsidebaropen]=useState(false)
+
 
 //   showing name:  
     const name=localStorage.getItem("name") 
+
 
     // logout function
  const logoutfc=()=>{
@@ -31,10 +36,10 @@ return(
 
 <div className="container-fluid">
 
-<div className="row bg-dark ">
+<div className="row bg-dark head-row  ">
 
 
-    <div class="col-lg-4">
+    <div class="col-lg-4 col-md-6">
 
 <div className="logo-section">
 <i className="fa-solid fa-bolt bolt-icon"></i>
@@ -43,34 +48,46 @@ return(
 
   <div className="webname">
       <h1 className="tittle">DOABA
-         <span className="hamb-bars"> <i className="fa-solid fa-bars" ></i>
 
+         <span className="hamb-bars" onClick={()=>setsidebaropen(true)}>
+           <i className="fa-solid fa-bars" ></i>
 </span>
 </h1>
         <h4 className="subtitle">STEEL FABRICATORS & WORKS</h4>
      </div>
 
 </div>
-
 </div>
 
-    <div class="col-lg-5 nav-link">
+   
+    <div class="col-lg-5 col-md-3 nav-link">
 
      <ul className="nav-name d-flex list-unstyled  ">
 <li><Link to={"/"}>Home</Link></li>
 <li><Link to={"/shop"}>Shop</Link></li>
 <li><Link to={"/about"}>About Us</Link></li>
-<li><Link to={"/signup"}>Account<i class="fa-solid fa-caret-down"></i> </Link></li>
+<li><Link to={"/signup"}>Account<i class="fa-solid fa-caret-down"></i> </Link>
+
+<div className="acount-drop">
+
+<ul className=" list-unstyled">
+
+<li><Link to={"/login"}Login></Link></li>
+<li><Link to={"/signup"}Registration></Link></li>
+
+</ul>
+
+</div>
+
+
+</li>
 <li><Link to={"/contact"}>Contact Us</Link></li>
 </ul>
 
-      
-     
-  
-    </div>
+ </div>
 
 
- <div class="col-lg-3 nav-user d-flex align-items-center px-5">
+ <div class="col-lg-3 col-md-3 nav-user d-flex align-items-center  px-5">
 
 
 <div className="nav-right">
@@ -119,7 +136,105 @@ return(
 </header> 
 
 
+{/* FOR SIDEBAR */}
 
+{sidebaropen && (
+  <>
+
+<div className="sidebar-overlay"
+ onClick={()=>setsidebaropen(false)}>
+</div>
+
+<div className="mobile-bar">
+
+  <div className="close-row">
+     
+      <button className="exit-icon" onClick={()=>setsidebaropen(false)}>
+    <i class="fa-solid fa-x"></i>
+  </button>
+
+  </div>
+
+  <div className="sidebar-top">
+
+    <h1>DOABA</h1>
+
+    <button className="loginbtn" onClick={() => {
+        if (name) {
+            logoutfc();
+        } else {
+            navigate("/login");
+        }
+    }}>
+        {name ? "Logout" : "Login"}
+        <i className="user-icon fa-solid fa-user" ></i>
+    </button>
+
+</div>
+
+  <h6 className="sidebar-name">
+    Welcome : <b>{name ? name : "Guest"}</b>
+</h6>
+
+{/* <div className="sidebar-top">
+  <h1>DOABA</h1>
+
+<button className="loginbtn">
+  Login<i className="user-icon fa-solid fa-user" ></i>
+</button>
+
+</div> */}
+
+
+{/* <h6 className="welcome-text">
+  Welcome: <b>{name || "Guest"}</b>
+
+  {name && (
+    <span>
+      <button className="logbtn" onClick={logoutfc}>
+        <i
+          className="fa-solid fa-right-from-bracket"
+        
+        ></i>
+      </button>
+    </span>
+  )}
+</h6> */}
+
+
+    <div className="sidebar-link">
+
+     <ul className="sidebar-nav  list-unstyled  ">
+<li><Link to={"/"}>Home</Link></li>
+<li><Link to={"/shop"}>Shop</Link></li>
+<li><Link to={"/about"}>About Us</Link></li>
+<li><Link to={"/contact"}>Contact Us</Link></li>
+</ul>
+
+ </div>
+
+
+
+<div className="sidebar-btn">
+<Link to={"/wishlist"}><button><b>Wishlist</b><i className="fa-solid fa-heart" ></i></button></Link>
+<button><b>search</b><i className="search-icon fa-solid fa-magnifying-glass" ></i></button>
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+</>
+
+
+)
+
+}
 
 
 
